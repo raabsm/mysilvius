3f365f49687a88a0c0e2224e3a2c82fcd8ec6a29
Sam <Raabsm@gmail.com>
2018-07-18 19:10:06 +0000

    prints out speech onto OLED screen first line, but text is cutoff after a few words because screen is so small

--- stream/mic.py
+++ stream/mic.py
@@ -8,6 +8,69 @@ import sys
 import urllib
 import json
 
+
+import time
+
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_SSD1306
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+import subprocess
+
+# Raspberry Pi pin configuration:
+RST = None     # on the PiOLED this pin isnt used
+# Note the following are only used with SPI:
+DC = 23
+SPI_PORT = 0
+SPI_DEVICE = 0
+
+# Beaglebone Black pin configuration:
+# RST = 'P9_12'
+# Note the following are only used with SPI:
+# DC = 'P9_15'
+# SPI_PORT = 1
+# SPI_DEVICE = 0
+
+# 128x32 display with hardware I2C:
+disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
+
+
+disp.begin()
+
+# Clear display.
+disp.clear()
+disp.display()
+
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+width = disp.width
+height = disp.height
+image = Image.new('1', (width, height))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+# Draw some shapes.
+# First define some constants to allow easy resizing of shapes.
+padding = -2
+top = padding
+bottom = height-padding
+# Move left to right keeping track of the current x position for drawing shapes.
+x = 0
+
+
+# Load default font.
+font = ImageFont.load_default()
+
+
+
+
 reconnect_mode = False
 fatal_error = False
 
@@ -109,6 +172,10 @@ class MyClient(WebSocketClient):
                 if response['result']['final']:
                     if self.show_hypotheses:
                         print >> sys.stderr, '\r%s' % trans.replace("\n", "\\n")
+                    draw.rectangle((0,0,width,height), outline=0, fill=0)
+                    draw.text((x, top),       trans,  font=font, fill=255)
+                    disp.image(image)
+                    disp.display()
                     print '%s' % trans.replace("\n", "\\n")  # final result!
                     sys.stdout.flush()
                 elif self.show_hypotheses:
